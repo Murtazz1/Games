f2f0b3959909b4a263c8061fa7e0686428985502
Murtazz1 <murikhalid.2525@live.com>
2020-10-29 22:02:02 -0400

    Revert "Revert "My first game! be nice :pray:""
    This reverts commit 6b6b6d17abe55095cb3509ca9b1866f8de12e2cf.

---
+++ Assets/Scripts/EnemyShots.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShots : MonoBehaviour
+{
+      public int spd = -5; // speed of beam
+    public int dmg; //Damage
+    private string target;
+    // Start is called before the first frame update
+    void Start() 
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, spd);
+    }
+
+    private void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        
+        if (other.tag == "Player" && other.tag == "PlayerBeam") {
+            Destroy(other.gameObject);  // kills other object if not player
+            Destroy(gameObject);        // beam goes away
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Shot : MonoBehaviour
+{
+    
+    public int spd = 5; // speed of beam
+    public int dmg; //Damage
+    public string target;
+    public GameObject explosion;
+    // Start is called before the first frame update
+    void Start() 
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, spd);
+    }
+
+    void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        
+        if (other.tag == target || other.tag == "PlayerBeam") {
+            if (target == "Enemy") {
+                Score.updateScore(100);
+            }
+            
+            Destroy(other.gameObject);  // kills other object if not player
+            GameObject fire = (GameObject) Instantiate(explosion, other.gameObject.transform.position, Quaternion.identity);
+            Destroy(fire, 0.4f);
+            Destroy(gameObject);        // beam goes away
+            if (other.tag == "Player") {
+                SceneManager.LoadScene(2);
+            }
+        }
+    }
+    
+    // Update is called once per frame
+    void Update() 
+    {
+        
+    }
+}
